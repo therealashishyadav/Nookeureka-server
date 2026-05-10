cea340f7206690713275006b1c0a4db8473d868b
Ashish Yadav <ashishyadav01111998@gmail.com>
2026-05-10 19:18:35 +0530

    Add application properties 3

--- src/main/java/com/EurekaServer/EurekaServerApplication.java
+++ src/main/java/com/EurekaServer/EurekaServerApplication.java
@@ -3,11 +3,20 @@ package com.EurekaServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaServer
+@RestController
 public class EurekaServerApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "pong";
     }
 }
